1c2f00e3c7c04f1f68aa402de00bd6f74eb84b01
Lucas <lfcarrega@gmail.com>
2026-04-04 06:39:29 +0000

    Add files via upload

---
+++ samba/setup_system.ps1
@@ -0,0 +1,13 @@
+# disable password complexity
+secedit /export /cfg C:\secpol.cfg
+(Get-Content C:\secpol.cfg) -replace "PasswordComplexity = 1", "PasswordComplexity = 0" | Set-Content C:\secpol.cfg
+secedit /configure /db C:\Windows\security\local.sdb /cfg C:\secpol.cfg /quiet
+Remove-Item C:\secpol.cfg
+
+# enable builtin admin
+net user Administrator /active:yes
+net user Administrator "inxad7nncm"
+
+# remove itself when done
+Remove-Item "C:\setup_system.ps1" -Force
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v Setup /f
